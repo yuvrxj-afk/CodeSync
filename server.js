8993954b642518c991120742cd8eadba1c478f72
uv-07 <63532070+uv-07@users.noreply.github.com>
2023-07-04 16:30:07 +0530

    scripts

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const http = require('http');
+const path = require('path');
 const { Server } = require('socket.io');
 const ACTIONS = require('./src/actions');
 const dotenv = require('dotenv');
@@ -9,6 +10,11 @@ const dotenv = require('dotenv');
 dotenv.config();
 const server = http.createServer(app);
 const io = new Server(server);
+
+app.use(express.static('build'))
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
 
 const PORT = process.env.PORT || 5000;
 
